4c56cb941dc3a2be064450544fed09a89833cb7b
bkaki <bhaskarkakii77@gmail.com>
2023-05-09 08:55:14 +0530

    terraform

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "catalogue-dev.bhaskar77.online:8080/"},
+  { name = "dev.frontend.cart_url", value = "cart-dev.bhaskar77.online:8080/"}
+]
--- env-prod/main.tf
+++ env-prod/main.tf
@@ -1,7 +1,8 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "String"
-  value = "Hello world"
-
+resource "aws_ssm_parameter" "parameters" {
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
+  type   = "String"
+  key_id = "6cdab819-cbbe-4a31-9fb9-40cefb302d8c"
 }
 
